652128f0b497d5cbecc6620833be537d0feb0cf9
Davi Gomes <davilucsg@gmail.com>
2019-02-25 10:43:25 -0300

    Aplicacao MVC minima utilizando a implementacao do ElasticSearch no componente Serilog. Contem tambem comandos para instancias containerizadas do ElasticSearch e Kibana.

---
+++ ELK/src/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using elastic_kibana.Models;
+using Microsoft.Extensions.Logging;
+
+namespace elastic_kibana.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            _logger.LogInformation($"GC time campeao! : {DateTime.UtcNow}");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
